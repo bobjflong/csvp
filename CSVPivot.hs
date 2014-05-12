a1cf983c54ed05d51a18639adfef6585217a5bff
Bob Long <robertjflong@gmail.com>
2014-05-12 23:31:50 +0100

    Progress: parsing csvs into PossibleNumbers, type-checking avg

--- CSVPivot.hs
+++ CSVPivot.hs
@@ -5,8 +5,14 @@ import Control.Lens
 import System.Environment
 import Text.CSV
 import Debug.Trace
+import System.IO
+import Data.Maybe
 
 ------------------------------------
+-- Parsing the DSL for manipulating CSVs
+-- eg. 'groupby 2; groupby 1; avg 3;'
+--
+
 import Text.ParserCombinators.Parsec
 
 data Command = Grouper Int |
@@ -28,28 +34,57 @@ parseTransformer f s =
      terminator
      return $ f $ read groupIx
 
-commandPossibilies = (try $ parseTransformer Averager "avg") <|> (try $ parseTransformer Grouper "group")
+commandSummarizePossibilities = try $ parseTransformer Averager "avg"
+
+commandGroupPossibilities = try $ parseTransformer Grouper "group"
 
 parseCommands :: GenParser Char st Command
 parseCommands =
-  do result <- many $ commandPossibilies
-     return $ CommandList result
+  do groupResult <- many $ commandGroupPossibilities
+     summarizeResult <- many $ commandSummarizePossibilities
+     return $ CommandList $ groupResult ++ summarizeResult
 
-parseCommandsFromArgs :: [String] -> [Maybe Command]
+parseCommandsFromArgs :: [String] -> [Either ParseError Command]
 parseCommandsFromArgs args = map performParse args
-  where performParse arg = preview _Right $ parse parseCommands source arg
+  where performParse arg = parse parseCommands source (arg ++ ";")
 
 ------------------------------------
+-- Parsing the CSV and manipulating it
+--
 
--- 2 dimensional average over csv 
-avgBy :: Int -> [[Double]] -> Double
-avgBy i lst = flip (/) len $ foldl (+) 0.0 $ lst^..traverse.ix i
-  where len = (fromIntegral $ length lst) :: Double
+parseStdinCSV :: String -> Either ParseError CSV
+parseStdinCSV csv = parseCSV source csv
 
-process :: String -> String
-process x = x
+type PossibleNumber = Either String Double
+type PossibleNumberCSV = [[PossibleNumber]]
+
+numberFromMaybe (Just x) = x
+numberFromMaybe _        = error "Error: could not convert to number"
+
+string2PossibleNumber :: String -> PossibleNumber
+string2PossibleNumber str = case (reads str) :: [(Double, String)] of
+  [(a, "")] -> Right a
+  _         -> Left str
+
+-- 2 dimensional average over CSV
+-- Throws an error if the user tries to summarize by a string
+avgBy :: Int -> PossibleNumberCSV -> Double
+avgBy i lst = flip (/) len $ summed
+  where len = (fromIntegral $ length converted) :: Double
+        summed = sum converted
+        converted = lst^..traverse.ix i^..traverse.to (preview _Right).to numberFromMaybe
+
+csvToPossibleNumbers :: CSV -> PossibleNumberCSV
+csvToPossibleNumbers csv = mapped %~ mapped %~ string2PossibleNumber $ csv
 
 main =
-  do commands <- getArgs
-     putStrLn $ show $ parseCommandsFromArgs commands
-     interact process
+  do csv <- getContents
+     case parseStdinCSV csv of
+      Left err -> do hPutStr stderr $ show err
+      Right parsed -> do
+       -- let converted = mapped %~ (map string2PossibleNumber) $ csv 
+        commands <- getArgs
+      -- putStrLn $ show $ parseCommandsFromArgs commands
+        putStrLn $ show $ avgBy 2 (csvToPossibleNumbers parsed)
+
+
